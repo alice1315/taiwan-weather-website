6318745ca01b9473006e2b1cb48ee5d198035a93
Alice Huang <viento.a13@gmail.com>
2022-04-21 18:45:17 +0800

    Optimized coding.

--- js/city.js
+++ js/city.js
@@ -1,5 +1,5 @@
 var data;
-var temp_weather_data;
+var tempWeatherData;
 var city;
 var cityNumber;
 var cityNumberOfIndexData;
@@ -9,13 +9,12 @@ async function cityInit(){
     tempWeatherData = await initIndexData();
     getCity();
     renderPage();
+    toIndex();
 }
 
 function getCity(){
     let url = new URL(window.location.href);
     city = url.searchParams.get("city");
-    // For test
-    icon = url.searchParams.get("icon");
     cityNumber = getCityNumberOfCityData(city);
     cityNumberOfIndexData = getCityNumberOfIndexData(city);
 }
@@ -24,20 +23,27 @@ function renderPage(){
     let cityName = document.querySelector(".city-name > span");
     let cityWeather = document.querySelector(".city-weather");
     let cityWeatherIcon = document.querySelector(".city-weather-icon-img");
-    let cityWinddirInfo = document.querySelector(".city-WindDir-info");
+    let cityWdsdInfo = document.querySelector(".city-WindDir-info");
     let cityAPInfo = document.querySelector(".city-AP-info");
     let cityRHInfo = document.querySelector(".city-RH-info");
     // 塞入圖片需要的變數
     let city_section = document.querySelector(".city-section");
 
     cityName.textContent = city;
-    cityWeather.textContent = temp_weather_data[cityNumberOfIndexData]["weatherElement"][2]["time"][0]["parameter"]["parameterName"];
-    cityWeatherIcon.src = icon;
-    cityWinddirInfo.textContent = data[cityNumber]["weatherElement"][1]["elementValue"];
-    cityAPInfo.textContent = data[cityNumber]["weatherElement"][5]["elementValue"];
-    cityRHInfo.textContent = data[cityNumber]["weatherElement"][4]["elementValue"];
+    cityWeather.textContent = tempWeatherData[cityNumberOfIndexData]["weatherElement"][2]["time"][0]["parameter"]["parameterName"] + " °C";
+    // cityWeatherIcon.src = icon;
+    cityWdsdInfo.textContent = data[cityNumber]["weatherElement"][2]["elementValue"] + " m/s";
+    cityAPInfo.textContent = data[cityNumber]["weatherElement"][5]["elementValue"] + " hPa";
+    cityRHInfo.textContent = data[cityNumber]["weatherElement"][4]["elementValue"] + " %";
     // 塞入圖片
     city_section.style.backgroundImage = "url('../static/pics/"+cityNumber+".jpg')"
     city_section.style.objectFit = "cover";
+}
+
+function toIndex(){
+    let btn = document.getElementById("city-button");
+    btn.addEventListener("click", function(){
+        location.href = "../html/index.html";
+    })
 }
 
