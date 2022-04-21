4dadfe413cc7948d88ad2d810c2ab609808d5e0a
qh70 <qh7047789@gmail.com>
2022-04-21 17:50:44 +0800

    增加新竹市、嘉義市，init_data.js改變數/函式名、新增一個getIcon，city.js小改+新增放出縣市照片，init_data新增函式getIndex，static/pics改檔名成縣市代碼。

--- js/city.js
+++ js/city.js
@@ -2,10 +2,11 @@ var data;
 var temp_weather_data;
 var city;
 var cityNumber;
-var cityNumber_of_index_data;
+var cityNumberOfIndexData;
 
 async function cityInit(){
-    data = await initData();
+    data = await initCityData();
+    tempWeatherData = await initIndexData();
     getCity();
     renderPage();
 }
@@ -13,21 +14,30 @@ async function cityInit(){
 function getCity(){
     let url = new URL(window.location.href);
     city = url.searchParams.get("city");
-    cityNumber = getCityNumber(city);
-    cityNumber_of_index_data = getCityNumber_of_index_data(city);
-    console.log(city, cityNumber, cityNumber_of_index_data)
+    // For test
+    icon = url.searchParams.get("icon");
+    cityNumber = getCityNumberOfCityData(city);
+    cityNumberOfIndexData = getCityNumberOfIndexData(city);
 }
 
 function renderPage(){
-    let cityName = document.querySelector(".city-name");
+    let cityName = document.querySelector(".city-name > span");
     let cityWeather = document.querySelector(".city-weather");
+    let cityWeatherIcon = document.querySelector(".city-weather-icon-img");
     let cityWinddirInfo = document.querySelector(".city-WindDir-info");
     let cityAPInfo = document.querySelector(".city-AP-info");
     let cityRHInfo = document.querySelector(".city-RH-info");
+    // 塞入圖片需要的變數
+    let city_section = document.querySelector(".city-section");
 
     cityName.textContent = city;
-    cityWeather.textContent = temp_weather_data[cityNumber_of_index_data]["weatherElement"][2]["time"][0]["parameter"]["parameterName"];
+    cityWeather.textContent = temp_weather_data[cityNumberOfIndexData]["weatherElement"][2]["time"][0]["parameter"]["parameterName"];
+    cityWeatherIcon.src = icon;
     cityWinddirInfo.textContent = data[cityNumber]["weatherElement"][1]["elementValue"];
     cityAPInfo.textContent = data[cityNumber]["weatherElement"][5]["elementValue"];
     cityRHInfo.textContent = data[cityNumber]["weatherElement"][4]["elementValue"];
+    // 塞入圖片
+    city_section.style.backgroundImage = "url('../static/pics/"+cityNumber+".jpg')"
+    city_section.style.objectFit = "cover";
 }
+
